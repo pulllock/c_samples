7faca4a65268d12fcec207b98eb960df5b822649
Pullock <pullocked@gmail.com>
2023-07-19 19:28:08 +0800

    pointer

---
+++ pointer_sample/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int* p;
+    char* q;
+    printf("%lu\n", sizeof (int*));
+    printf("%lu\n", sizeof (char*));
+    printf("%lu\n", sizeof (long*));
+    return 0;
+}
